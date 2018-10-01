38bc04739f66ec60ca3755bb677275a225eebf4a
cannakula-sfdc <catchmayya@gmail.com>
2018-10-01 13:52:22 -0700

    initial changes

---
+++ src/pages/index.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Link } from 'gatsby'
+
+import Layout from '../components/layout'
+
+const IndexPage = () => (
+  <Layout>
+    <h1>Hi people</h1>
+    <p>I work as a Software Engineer for Salesforce. I love learning new things and like to solve problems.</p>
+    <p>I wanted to take this opportunity to attend the hackathon and learn new things and meet new people.</p>
+    <p>I am passionate traveller, love listing to music and good at playing chess.</p>
+    <Link to="/page-2/">More about me</Link>
+  </Layout>
+)
+
+export default IndexPage
---
+++ src/pages/page-2.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Link } from 'gatsby'
+
+import Layout from '../components/layout'
+
+const SecondPage = () => (
+  <Layout>
+    <h1>Please feel free to connect me</h1>
+    <p>Linkedin: https://www.linkedin.com/in/chandravyas</p>
+    <Link to="/">Go back to the homepage</Link>
+  </Layout>
+)
+
+export default SecondPage
