5f1cd3a7d31311bbbb2d14d02c9d77e86dda2e34
cannakula-sfdc <catchmayya@gmail.com>
2018-10-01 14:22:05 -0700

    modified intro

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,8 +5,8 @@ import Layout from '../components/layout'
 
 const IndexPage = () => (
   <Layout>
-    <h1>Hi people</h1>
-    <p>I work as a Software Engineer for Salesforce. I love learning new things and like to solve problems.</p>
+    <h2>Hi people</h2>
+    <p>This is Chandra.I currently work as a Software Engineer for Salesforce. I love learning new things and like to solve problems.</p>
     <p>I wanted to take this opportunity to attend the hackathon and learn new things and meet new people.</p>
     <p>I am passionate traveller, love listing to music and good at playing chess.</p>
     <Link to="/page-2/">More about me</Link>
